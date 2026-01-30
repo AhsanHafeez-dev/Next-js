d07a0cf516b079ee87c1bd8af6fff2a4ed06866d
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2026-01-30 15:33:07 +0500

    learning next-js

---
+++ jsmastery/components/hello.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+
+
+function Hello() {
+    
+
+    console.log("i am client sude ")
+    return (
+        <>
+            <div>
+                iam from helloe
+            </div>
+        </>   
+    )
+}
+
+export default Hello
